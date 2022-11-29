9eae65228efcccaa0f4633f87b690b50373fdf05
dmitriykosenkov <dmkos23011991@gmail.com>
2022-11-29 17:59:17 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import s from './App.module.scss';
+import icon from './assets/icon-dice.svg'
+import divider from './assets/pattern-divider-desktop.svg'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [advice, setAdvice] = useState("");
+   const [fetching, setFetching] = useState(false);
+   useEffect(() => {
+      const fetchData = async () => {
+         const data = await axios.get("https://api.adviceslip.com/advice")
+         setAdvice(data.data.slip);
+         setFetching(false)
+      }
+      fetchData()
+   }, [fetching]);
+   console.log(advice);
+   return (
+      <div className={s.wrapper}>
+         <div className={s.container}>
+            <div className={s.card}>
+               <div className={s.cardInner}>
+                  <div className={s.cardSubtitle}>Advice #{advice.id}</div>
+                  <div className={s.cardTitle}>{advice.advice}</div>
+                  <div className={s.cardDivider}><img src={divider} alt="" /></div>
+                  <div className={s.cardIcon} onClick={() => setFetching(true)}>
+                     <img src={icon} alt="" />
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   );
 }
 
 export default App;
